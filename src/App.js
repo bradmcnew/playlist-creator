0f840b4035005a294bffb01a6f51515167a722e5
Brad McNew <mcnewbrad@gmail.com>
2024-06-20 23:19:58 -0400

    save to spotify button update

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ import Spotify from './api/spotify';
 
 function App() {
   const [searchResults, setSearchResults] = useState([
-    {name: 'song1', artist: 'artist1', album: 'album1', id: 1, uri: 'spotify:track:1'},
-    {name: 'song2', artist: 'artist2', album: 'album2', id: 2, uri: 'spotify:track:2'},
-    {name: 'song3', artist: 'artist3', album: 'album3', id: 3, uri: 'spotify:track:3'},
+    {name: 'Search for a song!', artist: 'artist', album: 'album', id: 1, uri: 'spotify:track:1'},
 ]);
   const [playlistTracks, setPlaylistTracks] = useState([]);
   const [playlistName, setPlaylistName] = useState('New Playlist');
@@ -34,6 +32,9 @@ function App() {
     }));
   }, [playlistTracks]);
   const savePlaylist = () => {
+    if (playlistTracks.length < 1) {
+      return;
+    }
     const trackURIs = playlistTracks.map(track => track.uri);
     Spotify.savePlaylist(playlistName, trackURIs).then(() => {
       setPlaylistName("New Playlist");
