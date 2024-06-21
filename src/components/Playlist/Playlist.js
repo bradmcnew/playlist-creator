1e35a0934d067b74ba3eae82e2c3723ebf9f185c
Brad McNew <mcnewbrad@gmail.com>
2024-06-20 21:31:03 -0400

    Playlist naming

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,15 +1,35 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import styles from "./Playlist.module.css";
 import Tracklist from "../Tracklist/Tracklist";
 
 const Playlist = ({playlist}) => {
     const [playlistName, setPlaylistName] = useState('New Playlist');
+    const [isDefault, setIsDefault] = useState(true);
     const handleChange = (e) => {
         setPlaylistName(e.target.value);
+        setIsDefault(false);
+    }
+    const handleFocus = () => {
+        if (isDefault) {
+            setPlaylistName('');
+            setIsDefault(false);
+        }
+    }
+    const handleBlur = () => {
+        if (playlistName.trim() === '') {
+            setPlaylistName('New Playlist');
+            setIsDefault(true);
+        }
     }
     return (
         <div className={styles.playlist}>
-            <input className={styles.name} onChange={handleChange} value={playlistName}></input>
+            <input 
+                className={styles.name} 
+                onChange={handleChange} 
+                onFocus={handleFocus} 
+                onBlur={handleBlur} 
+                value={playlistName}
+            />
             <Tracklist tracks={playlist} context={"playlist"} playlist={playlist}/>
             <button className={styles.save}>SAVE TO SPOTIFY</button>
         </div>
